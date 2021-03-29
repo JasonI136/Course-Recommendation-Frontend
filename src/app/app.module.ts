5028fa5611f38fcc690e7e21da3f64341699dc63
JasonI136 <63634201+JasonI136@users.noreply.github.com>
2021-03-30 01:57:34 +1100

    CSS Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,32 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IndexComponent } from './index/index.component';
-import { LoginComponent } from './accountManagement/login/login.component';
-import { RegisterationComponent } from './accountManagement/registeration/registeration.component';
-import { CourseRecommendationsComponent } from './courseManagement/course-recommendations/course-recommendations.component';
-import { CourseListComponent } from './courseManagement/course-list/course-list.component';
-import { CourseAreasComponent } from './courseManagement/course-areas/course-areas.component';
-import { SubjectManagementComponent } from './subject-management/subject-management.component';
-import { SubjectEntryComponent } from './subjectManagement/subject-entry/subject-entry.component';
-import { SubjectListComponent } from './subjectManagement/subject-list/subject-list.component';
-import { ContactUsComponent } from './subjectManagement/contact-us/contact-us.component';
+import { LoginComponent } from './components/Accounts/login/login.component';
 import { RegistrationComponent } from './components/Accounts/registration/registration.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { CourseAreasComponent } from './components/Course/course-areas/course-areas.component';
+import { CourseListComponent } from './components/Course/course-list/course-list.component';
+import { CourseRecommendationsComponent } from './components/Course/course-recommendations/course-recommendations.component';
+import { IndexComponent } from './components/index/index.component';
+import { SubjectEntryComponent } from './components/Subject/subject-entry/subject-entry.component';
+import { SubjectListComponent } from './components/Subject/subject-list/subject-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent,
+    RegistrationComponent,
     LoginComponent,
-    RegisterationComponent,
-    CourseRecommendationsComponent,
-    CourseListComponent,
-    CourseAreasComponent,
-    SubjectManagementComponent,
-    SubjectEntryComponent,
-    SubjectListComponent,
     ContactUsComponent,
-    RegistrationComponent
+    CourseAreasComponent,
+    CourseListComponent,
+    CourseRecommendationsComponent,
+    IndexComponent,
+    SubjectEntryComponent,
+    SubjectListComponent
   ],
   imports: [
     BrowserModule,
